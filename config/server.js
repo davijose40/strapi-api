be136f4af57b85d8ffa0216ccc819a77ccbd8eaa
Davi José <davijose40@gmail.com>
2022-09-08 10:38:46 -0300

    add git to project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd9da16790fa682a7981a68767c94c6cd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7193e9e-c406-40e9-b8e5-61ba5c0712f5'
+};
